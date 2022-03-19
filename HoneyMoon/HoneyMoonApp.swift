88a6694661d19a05401c6bc0cd267dfc7d6ebd40
marcoalonso <marcoalonsorota@hotmail.com>
2022-03-18 18:14:23 -0600

    GetingStartedWithAllResources

---
+++ HoneyMoon/HoneyMoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneyMoonApp.swift
+//  HoneyMoon
+//
+//  Created by marco rodriguez on 18/03/22.
+//
+
+import SwiftUI
+
+@main
+struct HoneyMoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
